c19f83153e78f142b329908b1744040789969cfa
Shweta <shwetaramdas@users.noreply.github.com>
2019-04-11 19:08:53 -0400

    Add files via upload
    Changed input options to include options of parallelization

--- runoverchr.py
+++ runoverchr.py
@@ -1,12 +1,23 @@
 #!/bin/python
 
+import re
 import os
-import sys
+import argparse
 
-command = sys.argv[1]
+parser = argparse.ArgumentParser()
+parser.add_argument("--command","-c",type=str)
+parser.add_argument("--parallel",action="store_true")
+
+args = parser.parse_args()
+command = args.command
 
 for chr in range(1, 23):
-        chrstr = str(chr)
-        command2 = command.replace("XXX", chrstr)
-        print(command2)
-        os.system(command2)
+	chrstr = str(chr)
+	command2 = command.replace("XXX", chrstr)
+	print(command2)
+#	os.system(command2)
+
+	if args.parallel:
+		os.system(command2 + " &")
+	else:
+		os.system(command2)
